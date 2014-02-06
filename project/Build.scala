4068db0012d0a3fac5c43f5f8d03321579843266
Olivier Samyn <olivier@26lights.com>
2014-02-06 10:24:54 +0100

    Publish to 26Lights repository.

--- project/Build.scala
+++ project/Build.scala
@@ -1,24 +1,37 @@
 import sbt._
 import sbt.Keys._
+import com.typesafe.sbt.SbtGit._
+
 
 object SlickDaoBuild extends Build {
 
   val buildName         = "SlickDao"
-  val appVersion        = "0.0.1-SNAPSHOT"
+  val appVersion        = "0.0.1"
   val scalaBuildOptions = Seq("-unchecked", "-deprecation", "-feature", "-language:reflectiveCalls")
 
+  val credentials26 = Credentials(Path.userHome / ".ivy2" / ".credentials")
+  val nexus = "http://build.26source.org/nexus/"
+  val snapshotRepo = Some("snapshots" at nexus + "content/repositories/snapshots")
+  val releaseRepo = Some("releases"  at nexus + "content/repositories/releases")
 
   val repos = Seq(
     "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
     "Typesafe snapshots"  at "http://repo.typesafe.com/typesafe/snapshots/",
     "Sonatype snapshots"  at "https://oss.sonatype.org/content/repositories/snapshots"
   )
-
+  
   val BuildSettings = Project.defaultSettings ++ Seq(
     scalaVersion := "2.10.3",
     scalacOptions := scalaBuildOptions,
     resolvers ++= repos,
-    version := appVersion
+    version := appVersion+"-"+git.gitHeadCommit.value.map{_.take(7)}.getOrElse("SNAPSHOT"),
+    publishTo := {
+      if (version.value.trim.endsWith("SNAPSHOT"))
+        snapshotRepo
+      else
+        releaseRepo
+    },
+    credentials += credentials26
   )
 
   val mainDependencies = Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,5 @@
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")
+
+resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven"
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.6.2")
